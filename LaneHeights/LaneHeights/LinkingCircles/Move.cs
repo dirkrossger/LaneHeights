8dc9c4047f0295493844c89a43f20579fe5648b0
dirkrossger <dirk.rossger@afconsult.com>
2017-07-07 08:25:19 +0200

    MoveCircle works

--- LaneHeights/LaneHeights/LinkingCircles/Move.cs
+++ LaneHeights/LaneHeights/LinkingCircles/Move.cs
@@ -17,44 +17,39 @@ namespace LaneHeights_LinkingCircles
 {
     public class Move
     {
-        [CommandMethod("MoveObject")]
+        [CommandMethod("MoveCircle")]
         public static void MoveObject()
         {
             // Get the current document and database
             Document acDoc = Application.DocumentManager.MdiActiveDocument;
             Database acCurDb = acDoc.Database;
+            Editor ed = acDoc.Editor;
 
             // Start a transaction
             using (Transaction acTrans = acCurDb.TransactionManager.StartTransaction())
             {
-                // Open the Block table for read
-                BlockTable acBlkTbl;
-                acBlkTbl = acTrans.GetObject(acCurDb.BlockTableId,
-                                                OpenMode.ForRead) as BlockTable;
+                PromptEntityOptions poCi = new PromptEntityOptions("\nSelect Circle! ");
+                poCi.SetRejectMessage("\nOnly Circle can be selected.");
+                poCi.AddAllowedClass(typeof(Circle), false);
+                PromptEntityResult res = ed.GetEntity(poCi);
 
-                // Open the Block table record Model space for write
-                BlockTableRecord acBlkTblRec;
-                acBlkTblRec = acTrans.GetObject(acBlkTbl[BlockTableRecord.ModelSpace],
-                                                OpenMode.ForWrite) as BlockTableRecord;
-
-                // Create a circle that is at 2,2 with a radius of 0.5
-                using (Circle acCirc = new Circle())
+                ObjectId from = res.ObjectId;
+                
+                if (res.Status == PromptStatus.OK)
                 {
-                    acCirc.Center = new Point3d(1, 1, 1);
-                    acCirc.Radius = 0.5;
+                    ObjectId id = res.ObjectId;
+                    Circle acCirc = acTrans.GetObject(id, OpenMode.ForWrite) as Circle;
 
-                    // Create a matrix and move the circle using a vector from (0,0,0) to (2,0,0)
-                    Point3d acPt3d = new Point3d(1, 1, 1);
-                    Vector3d acVec3d = acPt3d.GetVectorTo(new Point3d(2, 2, 2));
+                    PromptPointOptions pos = new PromptPointOptions("\nGet new position!");
 
+                    // Create a matrix and move the circle using a vector from cCenter to acPt3d
+                    PromptPointResult pr = ed.GetPoint(pos);
+                    Point3d acPt3d = pr.Value;
+                    Point3d cCenter = acCirc.Center;
 
-                    Vector3d vec = acCirc.Center - acPt3d;
-
+                    Vector3d acVec3d = cCenter.GetVectorTo(acPt3d);
+                    
                     acCirc.TransformBy(Matrix3d.Displacement(acVec3d));
-
-                    // Add the new object to the block table record and the transaction
-                    acBlkTblRec.AppendEntity(acCirc);
-                    acTrans.AddNewlyCreatedDBObject(acCirc, true);
                 }
 
                 // Save the new objects to the database
